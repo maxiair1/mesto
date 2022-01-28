ba0c52610444f13ae2c76669647023d5d648f524
Maxiair <maxiair@yandex.ru>
2022-01-28 19:05:40 +0300

    fix: bem naming, popup close by area, size profile elements fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,9 @@
 const popup = document.querySelector('.popup');
-const popupContainer = document.querySelector('popup__form')
+const popupForm = document.querySelector('.popup__form')
 const buttonClosePopup = document.querySelector('.popup__button-close');
-const buttonSubmitPopupForm = document.querySelector('.popup__button-submit');
 const buttonEditProfile = document.querySelector('.profile__button-edit');
-const buttonAddProfile = document.querySelector('.profile__button-add');
-const inputName = document.querySelector('.popup__input_name');
-const inputAbout = document.querySelector('.popup__input_about');
+const inputName = document.querySelector('.popup__input_field_name');
+const inputAbout = document.querySelector('.popup__input_field_about');
 const profileName = document.querySelector('.profile__title');
 const profileAbout = document.querySelector('.profile__subtitle');
 
@@ -15,27 +13,11 @@ const editProfile = () => {
   openPopup();
 };
 
-const addProfile = () => {
-  inputName.value = '';
-  inputAbout.value = '';
-  inputName.setAttribute('placeholder', 'Введите имя');
-  inputAbout.setAttribute('placeholder', 'О себе');
-  openPopup();
-  buttonSubmitPopupForm.addEventListener('click', submitForm);
-
-};
-
 const submitForm = (evt) => {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
   closePopup();
-};
-
-const closePopupByClickArea = (e) => {
-  if(e.target === e.currentTarget) {
-    closePopup();
-  }
 };
 
 const openPopup = () => {
@@ -46,8 +28,7 @@ const closePopup = () => {
   popup.classList.remove('popup_opened');
 };
 
-popup.addEventListener('click', closePopupByClickArea)
 buttonEditProfile.addEventListener('click', editProfile);
-// buttonAddProfile.addEventListener('click', addProfile);
 buttonClosePopup.addEventListener('click', closePopup);
-buttonSubmitPopupForm.addEventListener('click', submitForm);
+popupForm.addEventListener('submit', submitForm);
+
