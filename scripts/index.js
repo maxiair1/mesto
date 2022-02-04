b189f218975ccf3428017a8af34f2d0ab912dcbc
Maxiair <maxiair@yandex.ru>
2022-02-04 10:41:03 +0300

    feat: create template + render cards

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,44 @@ const inputName = document.querySelector('.popup__input_field_name');
 const inputAbout = document.querySelector('.popup__input_field_about');
 const profileName = document.querySelector('.profile__title');
 const profileAbout = document.querySelector('.profile__subtitle');
+const elements = document.querySelector('.elements');
+const elementTemplate = document.querySelector('.template-card').content;
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const renderCards = (elem) => {
+  elem.forEach( item => {
+    const elementClone = elementTemplate.cloneNode(true);
+    elementClone.querySelector('.element__title').innerText = item.name;
+    elementClone.querySelector('.element__image').src = item.link;
+    elementClone.querySelector('.element__image').alt = item.name;
+    elements.appendChild(elementClone);
+  })
+};
 
 const editProfile = () => {
   inputName.value = profileName.textContent;
@@ -27,7 +65,7 @@ const openPopup = () => {
 const closePopup = () => {
   popup.classList.remove('popup_opened');
 };
-
+renderCards(initialCards);
 buttonEditProfile.addEventListener('click', editProfile);
 buttonClosePopup.addEventListener('click', closePopup);
 popupForm.addEventListener('submit', submitForm);
